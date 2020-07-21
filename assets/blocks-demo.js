c9901fe59d7163f9fc58ecd356ab5b82f4b4103e
michaschwab <me@michailschwab.com>
2020-07-21 18:05:37 -0400

    add code highlighting plus comments

--- assets/blocks-demo.js
+++ assets/blocks-demo.js
@@ -1,145 +1,64 @@
-function loadGist()
-{
-    var idOnly = getGistId();
+angular.module('visconnectDemos', []).controller('visconnectSingleDemoController', function ($scope) {
+    $scope.init = () => {
+        if(window.location.pathname.length > 3)
+        {
+            $scope.loadGist();
+            var idOnly = $scope.getGistId();
 
-    //document.getElementById('block-link').setAttribute('href', 'https://bl.ocks.org/' + idOnly);
+            const demosAndInteractions = examples.concat(interactions);
 
-    d3.select('#iframe')
-        .attr('src', window.location.origin + '/' + idOnly + '/raw/index.html');
-}
-/*
-function addInteractivity()
-{
-    var iframeElementx = document.getElementById("iframe"),
-        iframeElementy = (iframeElementx.contentWindow || iframeElementx.contentDocument),
-        iframeElementz = iframeElementy.document.body;
-
-    var iframe = d3.select(iframeElementz);
-
-    iframe.append('script')
-        .attr('src', 'https://code.easypz.io/easypz.latest.js');
-    //.attr('src', '/easypz.js');
-
-    iframe.append('link')
-        .attr('rel', 'stylesheet')
-        .attr('href', '../../demo-styles.css');
-
-    setSettings();
-}*/
-/*
-function setSettings(modes, options, modeSettings, other) {
-    var iframeElementx = document.getElementById("iframe"),
-        iframeElementy = (iframeElementx.contentWindow || iframeElementx.contentDocument),
-        iframeElementz = iframeElementy.document.body;
-
-    var iframe = d3.select(iframeElementz);
-
-    var svgs = iframe.selectAll('svg');
-    var easypzSettings = getSettingsObject(modes, options, modeSettings);
-
-    if(other) {
-        for(let key in other) {
-            if(other.hasOwnProperty(key))
-                easypzSettings[key] = other[key];
+            var demosFiltered = demosAndInteractions.filter(function(d) { return d.blockid === idOnly });
+            if (demosFiltered.length === 1)
+            {
+                $scope.exampleInfo = demosFiltered[0];
+                $scope.addExampleInfo();
+            }
         }
     }
 
-    var easyPzString = JSON.stringify(easypzSettings);
+    $scope.loadGist = () => {
+        var idOnly = $scope.getGistId();
 
-    svgs.attr('easypz', easyPzString);
-}*/
+        //document.getElementById('block-link').setAttribute('href', 'https://bl.ocks.org/' + idOnly);
 
-function bodyLoaded()
-{
-    if(window.location.pathname.length > 3)
-    {
-        loadGist();
-
-        var idOnly = getGistId();
-
-        var demosFiltered = demos.filter(function(d) { return d.blockid === idOnly });
-        if (demosFiltered.length === 1)
-        {
-            document.getElementById('title').innerText += ': ' + demosFiltered[0].name;
-        }
+        d3.select('#iframe')
+            .attr('src', window.location.origin + '/' + idOnly + '/raw/index.html');
     }
-}
 
+    $scope.addExampleInfo = () => {
+        var idOnly = $scope.getGistId();
+        d3.select('#code-block').attr('data-src', idOnly + '/raw/index.html');
 
-document.addEventListener("DOMContentLoaded", function()
-{
-    var iframeEl = document.getElementById('iframe');
-    var maxtries = 100;
-    var tries = 0;
-
-    var trial = function()
-    {
-        tries++;
-
-        if(tries < maxtries)
-        {
-            var iframeElementx = document.getElementById("iframe"),
-                iframeElementy = (iframeElementx.contentWindow || iframeElementx.contentDocument),
-                body = iframeElementy.document.body;
-
-            if(iframeEl.src && body && body.querySelector('svg'))
-            {
-                //addInteractivity();
-            }
-            else
-            {
-                window.setTimeout(trial, 50);
-            }
+        if(!$scope.exampleInfo) {
+            return;
         }
-        else
-        {
-            console.error('no iframe src!');
-        }
-    };
 
-    trial();
+        document.getElementById('title').innerText += ': ' + $scope.exampleInfo.name;
+
+        if($scope.exampleInfo.codeHighlights && $scope.exampleInfo.codeHighlights["index.html"]) {
+            //5,7,18,38
+            d3.select('#code-block').attr('data-line', $scope.exampleInfo.codeHighlights["index.html"]);
+        }
+
+        if($scope.exampleInfo.codeComments && $scope.exampleInfo.codeComments["index.html"]) {
+            $scope.codeCommentsIndex = $scope.exampleInfo.codeComments["index.html"];
+        }
+
+        setTimeout(() => {
+            Prism.fileHighlight();
+        }, 100);
+    }
+
+    $scope.getGistId = () => {
+        var href = window.location.href;
+        if(href[href.length-1] !== '/')
+        {
+            href += '/';
+        }
+        var base_url = window.location.origin;
+        var baseRemoved = href.substr(base_url.length + 1);
+        return baseRemoved.substr(0, baseRemoved.indexOf('/'));
+    }
+
+    $scope.init();
 });
-
-function getGistId() {
-    var href = window.location.href;
-    if(href[href.length-1] !== '/')
-    {
-        href += '/';
-    }
-    var base_url = window.location.origin;
-    var baseRemoved = href.substr(base_url.length + 1);
-    return baseRemoved.substr(0, baseRemoved.indexOf('/'));
-}
-
-angular.module('visconnectDemos', [], function ($compileProvider) {
-
-    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|javascript):/);
-
-})
-    .controller('visconnectDemosCustomSettingsController', function($scope, $sce)
-    {
-        /*$scope.demos = demos;
-        $scope.modeNames = modeNames;
-        $scope.modes = $scope.modeNames.map(function(settingName) {
-            return {
-                name: settingName,
-                active: defaultModes.indexOf(settingName) !== -1
-            };
-        });
-        $scope.activeModeNames = defaultModes;
-        $scope.showSettings = 'none'; // 'none' | 'panmodes' | 'zoommodes' | 'scalebounds' | 'modeoptions' | 'export';
-
-        $scope.capitalizeFirstLetter = function(string) {
-            return string.charAt(0).toUpperCase() + string.slice(1);
-        };
-
-        $scope.transformModeName = function(string) {
-            return $scope.capitalizeFirstLetter(string.replace(/_/g, ' ').toLowerCase());
-        };
-
-        $scope.map = function(arr, f) { return arr.map(f); };*/
-    });
-
-const demos = [
-    { name: 'Drag', blockid: '20b08f298684c4b0e95f5a4a6c35d0e1', imgUrl: 'https://user-images.githubusercontent.com/288117/88079151-5064f200-cb4b-11ea-9957-bfcac4bbd9af.png'},
-];
--- assets/list.js
+++ assets/list.js
@@ -1,35 +1,5 @@
 angular.module('visconnectDemos', [])
     .controller('visconnectDemosListController', function ($scope) {
-        $scope.examples = [
-            {
-                name: 'Streamgraph',
-                blockid: '4060954',
-                imgUrl: 'https://d3js.org/ex/stream.png'
-            },
-        ];
-
-        $scope.interactions = [
-          
-            {
-                name: 'Mouse Events',
-                blockid: 'a018a51a1a9d9b2fea4387c73215ba9e',
-                imgUrl: 'https://user-images.githubusercontent.com/13991410/88092056-e0ac3280-cb5d-11ea-8793-11029af2a629.PNG',
-            },
-            {
-                name: 'Drag',
-                blockid: '20b08f298684c4b0e95f5a4a6c35d0e1',
-                imgUrl: 'https://user-images.githubusercontent.com/288117/88079151-5064f200-cb4b-11ea-9957-bfcac4bbd9af.png',
-            },
-            {
-                name: 'Brushing',
-                blockid: '28ec42110dd88094540938793ce4dd50',
-                imgUrl: 'https://user-images.githubusercontent.com/13991410/88092538-a2634300-cb5e-11ea-9e71-1723b8150944.PNG',
-            },
-             {
-                name: 'Zoom',
-                blockid: '020760869ab8bbff1d96e836e02cc9be',
-                imgUrl: 'https://gist.github.com/dsaffo/020760869ab8bbff1d96e836e02cc9be/raw/e2152cc48a9e16054509201dac53f30a73e20391/thumbnail.png',
-            },
-            
-        ];
+        $scope.examples = examples;
+        $scope.interactions = interactions;
     });
