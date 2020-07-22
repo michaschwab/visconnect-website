ae7c771a9ad74444d6012d32c85530a28fa80723
michaschwab <me@michailschwab.com>
2020-07-22 15:47:58 -0400

    minor update to the examples (one rename plus some code formatting)

--- assets/examples.js
+++ assets/examples.js
@@ -1,82 +1,82 @@
 let examples = [
- 
+
 ];
 
 let interactions = [
-  {
-    name: 'Mouse Events',
-    blockid: 'a018a51a1a9d9b2fea4387c73215ba9e',
-    imgUrl: 'https://user-images.githubusercontent.com/13991410/88092056-e0ac3280-cb5d-11ea-8793-11029af2a629.PNG',
-    codeHighlights: {
-      'index.html': "8,9"
-    },
-    codeComments: {
-      'index.html': [
+    {
+        name: 'Mouse Events',
+        blockid: 'a018a51a1a9d9b2fea4387c73215ba9e',
+        imgUrl: 'https://user-images.githubusercontent.com/13991410/88092056-e0ac3280-cb5d-11ea-8793-11029af2a629.PNG',
+        codeHighlights: {
+            'index.html': "8,9"
+        },
+        codeComments: {
+            'index.html': [
                 "Line 8: Add the PeerJS Dependency",
                 "Line 9: Add the VisConnect Dependency",
-                ]
-    }
-  },
-  {
-    name: 'Drag',
-    blockid: '9129f44cea5aabd962b54e93b523e632',
-    imgUrl: 'https://user-images.githubusercontent.com/288117/88079151-5064f200-cb4b-11ea-9957-bfcac4bbd9af.png',
-    codeHighlights: {
-      'index.html': "5,6,23,43"
+            ]
+        }
     },
-    codeComments: {
-      'index.html': [
+    {
+        name: 'Drag',
+        blockid: '9129f44cea5aabd962b54e93b523e632',
+        imgUrl: 'https://user-images.githubusercontent.com/288117/88079151-5064f200-cb4b-11ea-9957-bfcac4bbd9af.png',
+        codeHighlights: {
+            'index.html': "5,6,23,43"
+        },
+        codeComments: {
+            'index.html': [
                 "Line 5: Add the PeerJS Dependency",
                 "Line 6: Add the VisConnect Dependency",
                 "Line 23: Replace d3.drag with vc.drag",
                 "Line 43: Highlight circles with the color of the collaborator to distinguish them"]
-    }
+        }
     },
-  {
-    name: 'Brushing',
-    blockid: 'b2d66e94bf90016cb285ebc9515ebc0a',
-    imgUrl: 'https://user-images.githubusercontent.com/13991410/88092538-a2634300-cb5e-11ea-9e71-1723b8150944.PNG',
-    codeHighlights: {
-      'index.html': "4,5,6,20,82"
-    },
-    codeComments: {
-      'index.html': [
+    {
+        name: 'Divide-and-Conquer Brush',
+        blockid: 'b2d66e94bf90016cb285ebc9515ebc0a',
+        imgUrl: 'https://user-images.githubusercontent.com/13991410/88092538-a2634300-cb5e-11ea-9e71-1723b8150944.PNG',
+        codeHighlights: {
+            'index.html': "4,5,6,20,82"
+        },
+        codeComments: {
+            'index.html': [
                 "Line 4: Add the collaberation='live' attribute to body tag",
                 "Line 5: Add the PeerJS Dependency",
                 "Line 6: Add the VisConnect Dependency",
                 "Line 20: Replace Math.random with vc.random",
                 "Line 82: Replace d3.brush with vc.brush",
-               
-                ]
-    }
-  },
-  {
-    name: 'Zoom',
-    blockid: '020760869ab8bbff1d96e836e02cc9be',
-    imgUrl: 'https://gist.github.com/dsaffo/020760869ab8bbff1d96e836e02cc9be/raw/e2152cc48a9e16054509201dac53f30a73e20391/thumbnail.png',
-        codeHighlights: {
-      'index.html': "37,38"
+
+            ]
+        }
     },
-    codeComments: {
-      'index.html': [
+    {
+        name: 'Zoom',
+        blockid: '020760869ab8bbff1d96e836e02cc9be',
+        imgUrl: 'https://gist.github.com/dsaffo/020760869ab8bbff1d96e836e02cc9be/raw/e2152cc48a9e16054509201dac53f30a73e20391/thumbnail.png',
+        codeHighlights: {
+            'index.html': "37,38"
+        },
+        codeComments: {
+            'index.html': [
                 "Line 37: Add the PeerJS Dependency",
                 "Line 38: Add the VisConnect Dependency",
-                ]
-    }
+            ]
+        }
     },
-     {
-    name: 'UI',
-    blockid: 'ff0f1abebb0a72520411940bf82cbedf',
-    imgUrl: 'https://user-images.githubusercontent.com/13991410/88219451-1e3db800-cc51-11ea-8003-7540dcbe550c.PNG',
+    {
+        name: 'UI',
+        blockid: 'ff0f1abebb0a72520411940bf82cbedf',
+        imgUrl: 'https://user-images.githubusercontent.com/13991410/88219451-1e3db800-cc51-11ea-8003-7540dcbe550c.PNG',
         codeHighlights: {
-      'index.html': "8,9,18"
-    },
-    codeComments: {
-      'index.html': [
+            'index.html': "8,9,18"
+        },
+        codeComments: {
+            'index.html': [
                 "Line 8: Add the PeerJS Dependency",
                 "Line 9: Add the VisConnect Dependency",
                 "Line 18: Add the collaberation='live' attribute to body tag",
-                ]
-    }
+            ]
+        }
     },
 ];
