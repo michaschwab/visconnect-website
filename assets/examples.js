cc6a7f9bd476577e6cfee04c68ca8e2f4a9f12e8
David Saffo <saffo.d@husky.neu.edu>
2020-07-22 15:43:38 -0400

    updated interaction examples with code lines and descriptions added ui example removed steamgraph left over

--- assets/examples.js
+++ assets/examples.js
@@ -1,41 +1,82 @@
 let examples = [
-    {
-        name: 'Streamgraph',
-        blockid: '4060954',
-        imgUrl: 'https://d3js.org/ex/stream.png'
-    },
+ 
 ];
 
 let interactions = [
-    {
-        name: 'Mouse Events',
-        blockid: 'a018a51a1a9d9b2fea4387c73215ba9e',
-        imgUrl: 'https://user-images.githubusercontent.com/13991410/88092056-e0ac3280-cb5d-11ea-8793-11029af2a629.PNG',
+  {
+    name: 'Mouse Events',
+    blockid: 'a018a51a1a9d9b2fea4387c73215ba9e',
+    imgUrl: 'https://user-images.githubusercontent.com/13991410/88092056-e0ac3280-cb5d-11ea-8793-11029af2a629.PNG',
+    codeHighlights: {
+      'index.html': "8,9"
     },
-    {
-        name: 'Drag',
-        blockid: '20b08f298684c4b0e95f5a4a6c35d0e1',
-        imgUrl: 'https://user-images.githubusercontent.com/288117/88079151-5064f200-cb4b-11ea-9957-bfcac4bbd9af.png',
-        codeHighlights: {
-            'index.html': "5,7,18,38"
-        },
-        codeComments: {
-            'index.html': [
+    codeComments: {
+      'index.html': [
+                "Line 8: Add the PeerJS Dependency",
+                "Line 9: Add the VisConnect Dependency",
+                ]
+    }
+  },
+  {
+    name: 'Drag',
+    blockid: '9129f44cea5aabd962b54e93b523e632',
+    imgUrl: 'https://user-images.githubusercontent.com/288117/88079151-5064f200-cb4b-11ea-9957-bfcac4bbd9af.png',
+    codeHighlights: {
+      'index.html': "5,6,23,43"
+    },
+    codeComments: {
+      'index.html': [
                 "Line 5: Add the PeerJS Dependency",
-                "Line 7: Add the VisConnect Dependency",
-                "Line 18: Replace d3.drag with vc.drag",
-                "Line 38: Highlight circles with the color of the collaborator to distinguish them"]
-        }
+                "Line 6: Add the VisConnect Dependency",
+                "Line 23: Replace d3.drag with vc.drag",
+                "Line 43: Highlight circles with the color of the collaborator to distinguish them"]
+    }
     },
-    {
-        name: 'Brushing',
-        blockid: '28ec42110dd88094540938793ce4dd50',
-        imgUrl: 'https://user-images.githubusercontent.com/13991410/88092538-a2634300-cb5e-11ea-9e71-1723b8150944.PNG',
+  {
+    name: 'Brushing',
+    blockid: 'b2d66e94bf90016cb285ebc9515ebc0a',
+    imgUrl: 'https://user-images.githubusercontent.com/13991410/88092538-a2634300-cb5e-11ea-9e71-1723b8150944.PNG',
+    codeHighlights: {
+      'index.html': "4,5,6,20,82"
     },
-    {
-        name: 'Zoom',
-        blockid: '020760869ab8bbff1d96e836e02cc9be',
-        imgUrl: 'https://gist.github.com/dsaffo/020760869ab8bbff1d96e836e02cc9be/raw/e2152cc48a9e16054509201dac53f30a73e20391/thumbnail.png',
+    codeComments: {
+      'index.html': [
+                "Line 4: Add the collaberation='live' attribute to body tag",
+                "Line 5: Add the PeerJS Dependency",
+                "Line 6: Add the VisConnect Dependency",
+                "Line 20: Replace Math.random with vc.random",
+                "Line 82: Replace d3.brush with vc.brush",
+               
+                ]
+    }
+  },
+  {
+    name: 'Zoom',
+    blockid: '020760869ab8bbff1d96e836e02cc9be',
+    imgUrl: 'https://gist.github.com/dsaffo/020760869ab8bbff1d96e836e02cc9be/raw/e2152cc48a9e16054509201dac53f30a73e20391/thumbnail.png',
+        codeHighlights: {
+      'index.html': "37,38"
     },
-
+    codeComments: {
+      'index.html': [
+                "Line 37: Add the PeerJS Dependency",
+                "Line 38: Add the VisConnect Dependency",
+                ]
+    }
+    },
+     {
+    name: 'UI',
+    blockid: 'ff0f1abebb0a72520411940bf82cbedf',
+    imgUrl: 'https://user-images.githubusercontent.com/13991410/88219451-1e3db800-cc51-11ea-8003-7540dcbe550c.PNG',
+        codeHighlights: {
+      'index.html': "8,9,18"
+    },
+    codeComments: {
+      'index.html': [
+                "Line 8: Add the PeerJS Dependency",
+                "Line 9: Add the VisConnect Dependency",
+                "Line 18: Add the collaberation='live' attribute to body tag",
+                ]
+    }
+    },
 ];
